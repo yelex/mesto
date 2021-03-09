a7304b3250837a7f59b122684ae180b18113a87f
Alexey Evseev <evseev_alexey94@bk.ru>
2021-03-09 13:41:34 +0300

    feat: add card removing

--- scripts/index.js
+++ scripts/index.js
@@ -113,6 +113,14 @@ const initialCards = [
   }
 ];
 
+function toggleLikeIcoCard(evt){
+  evt.target.classList.toggle('card__heart-ico_active');
+}
+
+function removeCard(evt){
+  evt.target.closest('.card').remove();
+}
+
 function initializeCards(initialCards){
   const cardList = document.querySelector('.cards__list');
   while (cardList.firstChild) {
@@ -123,6 +131,8 @@ function initializeCards(initialCards){
   initialCards.forEach(item => {
     const card = cardTemplate.querySelector('.card').cloneNode(true);
 
+    card.querySelector('.card__heart-ico').addEventListener('click', toggleLikeIcoCard);
+    card.querySelector('.card__trash-ico').addEventListener('click', removeCard);
     card.querySelector('.card__image').src = item.link;
     card.querySelector('.card__image').alt = item.name;
     card.querySelector('.card__title').textContent = item.name;
@@ -134,11 +144,8 @@ function initializeCards(initialCards){
 
 initializeCards(initialCards);
 
-function toggleLikeIcoCard(evt){
-  evt.target.classList.toggle('card__heart-ico_active');
-}
-const cardLikes = document.querySelectorAll('.card__heart-ico');
-cardLikes.forEach(item => {
-  item.addEventListener('click', toggleLikeIcoCard)
-})
+
+
+
+
 
