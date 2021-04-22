820a8cc8f3144b461ff0369e3c496d3833ebb0cb
Alexey Evseev <evseev_alexey94@bk.ru>
2021-04-22 23:50:18 +0300

    refactor: add popup

---
+++ scripts/components/Popup.js
@@ -0,0 +1,29 @@
+export default class Popup {
+  constructor(popupSelector) {
+    this._popup = document.querySelector(popupSelector);
+  }
+
+  open() {
+    this._popup.classList.add('popup_opened');
+    document.addEventListener('keydown', _handleEscClose);
+  }
+
+  close() {
+    this._popup.classList.remove('popup_opened');
+    document.removeEventListener('keydown', _handleEscClose);
+  }
+
+  _handleEscClose(evt) {
+    if (evt.key===ESC_KEY){
+      close();
+    }
+  }
+
+  setEventListeners() {
+    this._popup.addEventListener('click', function (evt) {
+      if (evt.target.classList.contains('popup_opened')||evt.target.classList.contains('popup__close-btn')) {
+        close(this._popup);
+      };
+    })
+  }
+}
