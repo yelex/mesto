bb7ba32339d114a8b053372db8edba5a55c8ffc0
Alexey Evseev <evseev_alexey94@bk.ru>
2021-04-25 14:53:21 +0300

    fix: add bind in listener to constructor in Popup.js

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -3,19 +3,21 @@ import { ESC_KEY } from '../utils/constants.js'
 export default class Popup {
   constructor(popupSelector) {
     this._popup = document.querySelector(popupSelector);
+    this._handleEscCloseBind = this._handleEscClose.bind(this);
   }
 
   open() {
     this._popup.classList.add('popup_opened');
-    document.addEventListener('keydown', this._handleEscClose.bind(this));
+    document.addEventListener('keydown', this._handleEscCloseBind);
   }
 
   close() {
     this._popup.classList.remove('popup_opened');
-    document.removeEventListener('keydown', this._handleEscClose.bind(this));
+    document.removeEventListener('keydown', this._handleEscCloseBind);
   }
 
   _handleEscClose(evt) {
+    console.log(evt.key)
     if (evt.key===ESC_KEY){
       this.close();
     }
